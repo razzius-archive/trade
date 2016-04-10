fcbd760e71efef6a813d21f3123f2382cfed6f92
Razzi Abuissa <razzi53@gmail.com>
2016-04-09 20:31:12 -0700

    Add tests and some implementation

--- trade/experiment.py
+++ trade/experiment.py
@@ -1,12 +1,68 @@
 from pandas import DataFrame, Series
+from pandas.tseries.index import DatetimeIndex
 
+
+# class DataDescriptor():
+#     """A description of a raw data value to be calculated.
+
+#     Encapsulates the identifier as well as any arguments used
+#     to parameterize the calculation.
+#     """
+#     def __init__(self, identifier, **parameters):
+#         self.identifier = identifier
+#         self.parameters = parameters
 
 class Strategy():
     pass
 
 
-def get_data(symbol, data_names, start_date, end_date=None) -> DataFrame:
-    pass
+# classes of metrics
+# - depend on symbol or not
+# - parameterized or not
+
+# temperature_data = Metric(stock_related=False, parametrized=True)
+
+# ? stock_metric
+# ? city is repeated here
+# @metric('temperature', required_parameters=['city'])
+# def _get_temperature_data(date_range: DateTimeIndex,
+#                           city) -> Series:
+#     # todo razzi
+#     raise NotImplementedError
+
+# temperature_data.execute
+
+def _get_temperature_data(date_range: DatetimeIndex) -> Series:
+    # todo razzi
+    raise NotImplementedError
+
+
+def _get_close_price(date_range, symbol='') -> Series:
+    # todo jon
+    raise NotImplementedError
+
+# for example
+# sf_temperature =('temperature', {'city': 'San Francisco'})
+# sf_temperature = DataDescriptor('temperature', city='San Francisco')
+# get_data('aapl', ['close', sf_temperature])
+
+
+def get_data(data_descriptors: dict,
+             date_range: DatetimeIndex) -> DataFrame:
+    """TODO document
+    """
+    data_functions = {
+        'close': _get_close_price,
+        'high_temperature': _get_temperature_data
+    }
+
+    # TODO pass params
+    # refactor
+
+    return DataFrame({
+        identifier: data_functions[descriptor['type']](date_range, **descriptor)
+        for identifier, descriptor in data_descriptors.values()
+    })
 
 
 def calculate_indicators(input_df, indicator_names) -> DataFrame:
--- trade/test_experiment.py
+++ trade/test_experiment.py
@@ -1,10 +1,82 @@
 import datetime
 
+import pandas as pd
+from pandas.tseries.index import DatetimeIndex
+
 from experiment import get_data, calculate_indicators, evaluate_strategy
 
 
-def test_get_data():
-    """Close price should come back as a float."""
-    close_price_df = get_data('AAPL', ['close'], datetime.date.today(), datetime.date.today())
+APRIL_1_2016_DATERANGE = pd.date_range(start=datetime.date(2016, 4, 1), periods=1)
+
+FIRST_WEEK_IN_APRIL = pd.date_range(start=datetime.date(2016, 4, 1), periods=7)
+
+
+def test_get_sf_high_temperature():
+    """High temperature for SF was 59 degrees Fahrenheit on April 1, 2016."""
+    temperature_df = get_data({'sf_temperature': {'type': 'high_temperature'}}, APRIL_1_2016_DATERANGE)
+
+    assert temperature_df.columns == ['sf_temperature']
+    assert len(temperature_df.columns) == 1
+
+    assert temperature_df['sf_temperature'].iloc[0] == 59
+
+
+def test_get_boston_high_temperature():
+    """High temperature for Boston was 69 degrees Fahrenheit on April 1, 2016."""
+    temperature_df = get_data({'boston_temperature': {'type': 'high_temperature'}}, APRIL_1_2016_DATERANGE)
+
+    assert temperature_df.columns == ['boston_temperature']
+    assert len(temperature_df.columns) == 1
+
+    assert temperature_df['boston_temperature'].iloc[0] == 69
+
+
+def test_get_apple_close_price_data():
+    """Apple close price for April 1, 2016 was $109.99."""
+    close_price_descriptor = {
+        'type': 'close',
+        'symbol': 'AAPL',
+    }
+
+    close_price_df = get_data({'apple_close': close_price_descriptor}, APRIL_1_2016_DATERANGE)
+
     value = close_price_df['close'].iloc[0]
-    assert isinstance(value, float)
+
+    assert value == 109.99
+
+
+def test_get_amazon_high_price():
+    """High price for amazon on 2016-04-01 was ???"""
+    close_price_descriptor = {
+        'type': 'close',
+        'symbol': 'AAPL',
+    }
+
+    close_price_df = get_data({'apple_close': close_price_descriptor}, APRIL_1_2016_DATERANGE)
+    value = close_price_df['close'].iloc[0]
+
+    # TODO find value
+
+
+def test_get_multiple_data_sources():
+    """Query historical temperatures for different cities."""
+    metrics = {
+        'sf_temperature': {'type': 'temperature', 'value': 'high_temperature', 'city': 'San Francisco'},
+        'boston_temperature': {'type': 'temperature', 'value': 'high_temperature', 'city': 'Boston'}
+    }
+
+    df = get_data(metrics, APRIL_1_2016_DATERANGE)
+
+    # TODO find values
+
+
+def test_get_close_data_for_range():
+    """Apple close data for the week of April 1 to April 8."""
+    close_price_descriptor = {
+        'type': 'close',
+        'symbol': 'AAPL',
+    }
+
+    close_price_df = get_data({'apple_close': close_price_descriptor}, FIRST_WEEK_IN_APRIL)
+
+    # TODO find values
