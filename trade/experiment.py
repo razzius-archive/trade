080c9818e7e0a40a41b10435d939ab9879ff7c0c
Razzi Abuissa <razzi53@gmail.com>
2016-04-10 17:44:34 -0700

    Add moon phase data function

--- trade/experiment.py
+++ trade/experiment.py
@@ -1,6 +1,8 @@
 from pandas import DataFrame, Series
 from pandas.tseries.index import DatetimeIndex
 
+import astral
+
 
 # class DataDescriptor():
 #     """A description of a raw data value to be calculated.
@@ -32,7 +34,19 @@ class Strategy():
 
 # temperature_data.execute
 
-def _get_temperature_data(date_range: DatetimeIndex) -> Series:
+def descriptor_parameters(descriptor):
+    """Returns a dictionary of parameters to be passed to a data calculation function.
+
+    Namely `type` is not a parameter, so remove it. (This could be redesigned)
+    """
+    return {
+        key: value
+        for key, value in descriptor.items()
+        if key != 'type'
+    }
+
+def _high_temperature_data(date_range: DatetimeIndex,
+                           city: None) -> Series:
     # todo razzi
     raise NotImplementedError
 
@@ -40,6 +54,15 @@ def _get_temperature_data(date_range: DatetimeIndex) -> Series:
 def _get_close_price(date_range, symbol='') -> Series:
     # todo jon
     raise NotImplementedError
+
+
+def _moon_phase(date_range: DatetimeIndex) -> Series:
+    almanac = astral.Astral()
+
+    return Series([
+        almanac.moon_phase(date)
+        for date in date_range
+    ], index=date_range)
 
 # for example
 # sf_temperature =('temperature', {'city': 'San Francisco'})
@@ -53,16 +76,32 @@ def get_data(data_descriptors: dict,
     """
     data_functions = {
         'close': _get_close_price,
-        'high_temperature': _get_temperature_data
+        'high_temperature': _high_temperature_data,
+        'moon_phase': _moon_phase
     }
 
-    # TODO pass params
     # refactor
 
+    """
+    input: {'the_moon_phase': {'type': 'moon_phase'}}
+
+    input.keys() -> ['the_moon_phase']
+    input.values() -> [{'type': 'moon_phase'}]
+
+    for val in values:
+        identifier = val['type'] -> 'moon_phase'
+        fn = data_functions[identifier] -> _moon_phase
+
+        output_data = fn(date_range) -> _moon_phase(date_range)
+         -> Series(14, 16, 17)
+        return output_data
+    """
+
     return DataFrame({
-        identifier: data_functions[descriptor['type']](date_range, **descriptor)
-        for identifier, descriptor in data_descriptors.values()
-    })
+        identifier: data_functions[descriptor['type']](
+            date_range, **descriptor_parameters(descriptor)
+        ) for identifier, descriptor in data_descriptors.items()
+    }, index=date_range)
 
 
 def calculate_indicators(input_df, indicator_names) -> DataFrame:
--- trade/test_experiment.py
+++ trade/test_experiment.py
@@ -13,32 +13,45 @@ FIRST_WEEK_IN_APRIL = pd.date_range(start=datetime.date(2016, 4, 1), periods=7)
 
 def test_get_sf_high_temperature():
     """High temperature for SF was 59 degrees Fahrenheit on April 1, 2016."""
-    temperature_df = get_data({'sf_temperature': {'type': 'high_temperature'}}, APRIL_1_2016_DATERANGE)
+    data_descriptors = {'sf_temperature': {'type': 'high_temperature', 'city': 'San Francisco'}}
+    temperature_df = get_data(data_descriptors, APRIL_1_2016_DATERANGE)
 
     assert temperature_df.columns == ['sf_temperature']
-    assert len(temperature_df.columns) == 1
 
     assert temperature_df['sf_temperature'].iloc[0] == 59
 
 
 def test_get_boston_high_temperature():
     """High temperature for Boston was 69 degrees Fahrenheit on April 1, 2016."""
-    temperature_df = get_data({'boston_temperature': {'type': 'high_temperature'}}, APRIL_1_2016_DATERANGE)
+    data_descriptors = {'boston_temperature': {'type': 'high_temperature', 'city': 'Boston'}}
+    temperature_df = get_data(data_descriptors, APRIL_1_2016_DATERANGE)
 
     assert temperature_df.columns == ['boston_temperature']
-    assert len(temperature_df.columns) == 1
 
     assert temperature_df['boston_temperature'].iloc[0] == 69
 
 
+def test_moon_phase():
+    """March 24, 2016 was a full moon."""
+    data_descriptors = {'moon_phase': {'type': 'moon_phase'}}
+    date_range = pd.date_range(start=datetime.date(2016, 3, 24), periods=1)
+
+    moon_phase_df = get_data(data_descriptors, date_range)
+
+    assert moon_phase_df.columns == ['moon_phase']
+    assert moon_phase_df.index == date_range
+
+    assert moon_phase_df['moon_phase'].iloc[0] == 14  # full moon
+
+
 def test_get_apple_close_price_data():
     """Apple close price for April 1, 2016 was $109.99."""
-    close_price_descriptor = {
+    close_price_parameters = {
         'type': 'close',
         'symbol': 'AAPL',
     }
 
-    close_price_df = get_data({'apple_close': close_price_descriptor}, APRIL_1_2016_DATERANGE)
+    close_price_df = get_data({'apple_close': close_price_parameters}, APRIL_1_2016_DATERANGE)
 
     value = close_price_df['close'].iloc[0]
 
@@ -47,12 +60,12 @@ def test_get_apple_close_price_data():
 
 def test_get_amazon_high_price():
     """High price for amazon on 2016-04-01 was ???"""
-    close_price_descriptor = {
+    close_price_parameters = {
         'type': 'close',
-        'symbol': 'AAPL',
+        'symbol': 'AMZN',
     }
 
-    close_price_df = get_data({'apple_close': close_price_descriptor}, APRIL_1_2016_DATERANGE)
+    close_price_df = get_data({'apple_close': close_price_parameters}, APRIL_1_2016_DATERANGE)
     value = close_price_df['close'].iloc[0]
 
     # TODO find value
@@ -61,8 +74,8 @@ def test_get_amazon_high_price():
 def test_get_multiple_data_sources():
     """Query historical temperatures for different cities."""
     metrics = {
-        'sf_temperature': {'type': 'temperature', 'value': 'high_temperature', 'city': 'San Francisco'},
-        'boston_temperature': {'type': 'temperature', 'value': 'high_temperature', 'city': 'Boston'}
+        'sf_temperature': {'type': 'high_temperature', 'city': 'San Francisco'},
+        'boston_temperature': {'type': 'high_temperature', 'city': 'Boston'}
     }
 
     df = get_data(metrics, APRIL_1_2016_DATERANGE)
